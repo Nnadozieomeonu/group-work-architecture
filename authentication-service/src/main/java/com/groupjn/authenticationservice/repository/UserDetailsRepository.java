b0a227b2c5d6131fe1660417a4aa75619fae3f8e
Nnadozie Omeonu <nnadozieome@gmail.com>
2021-04-11 19:33:59 +0100

    Implemented User controller, Bulder pattern, Factroy Factory

--- authentication-service/src/main/java/com/groupjn/authenticationservice/repository/UserDetailsRepository.java
+++ authentication-service/src/main/java/com/groupjn/authenticationservice/repository/UserDetailsRepository.java
@@ -2,9 +2,11 @@ package com.groupjn.authenticationservice.repository;
 
 import com.groupjn.authenticationservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserDetailsRepository extends JpaRepository<User,Integer> {
     Optional<User> findByUsername(String name);
 }
