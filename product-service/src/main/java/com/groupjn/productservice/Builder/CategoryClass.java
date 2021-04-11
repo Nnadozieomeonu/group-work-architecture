ef3616fca799fd4313c9222d164711bb2eaba143
Nnadozie Omeonu <nnadozieome@gmail.com>
2021-04-11 19:56:45 +0100

    merged with joys branch  and fixed bug

--- product-service/src/main/java/com/groupjn/productservice/Builder/CategoryClass.java
+++ product-service/src/main/java/com/groupjn/productservice/Builder/CategoryClass.java
@@ -1,43 +1,17 @@
 package com.groupjn.productservice.Builder;
 
+import lombok.Data;
+
+@Data
 public class CategoryClass {
     //All final attributes
     private final String name; // required
     private final String description; // required
 
-    private CategoryClass(CategoryBuilder ct) {
-        this.name = ct.name;
-        this.description = ct.description;
+    public CategoryClass(CategoryBuilder ct) {
+        this.name = ct.getName();
+        this.description = ct.getDescription();
     }
 
-    //All getter, and NO setter to provde immutability
-    public String getName() {
-        return name;
-    }
-    public String getDescription() {
-        return description;
-    }
 
-    public static class CategoryBuilder
-    {
-        private final String name;
-        private String description;
-
-        public CategoryBuilder(String name) {
-            this.name = name;
-        }
-        public CategoryBuilder description(String description) {
-            this.description = description;
-            return this;
-        }
-        //Return the finally consrcuted User object
-        public CategoryClass build() {
-            CategoryClass cat =  new CategoryClass(this);
-            return cat;
-        }
-        private void validateUserObject(CategoryClass user) {
-            //Do some basic validations to check
-            //if user object does not break any assumption of system
-        }
-    }
 }
--- product-service/src/main/java/com/groupjn/productservice/controller/ProductCategoryController.java
+++ product-service/src/main/java/com/groupjn/productservice/controller/ProductCategoryController.java
@@ -1,9 +1,12 @@
 package com.groupjn.productservice.controller;
 
+import com.groupjn.productservice.Builder.CategoryBuilder;
 import com.groupjn.productservice.Builder.CategoryClass;
 import com.groupjn.productservice.api.command.AddProductCommand;
 import com.groupjn.productservice.entity.ProductSummary;
 import com.groupjn.productservice.model.Category;
+import com.groupjn.productservice.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("category")
+@RequestMapping("product/category")
 public class ProductCategoryController {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @PostMapping("/addCategory")
-    public void handle(@RequestBody Category cat) {
-        CategoryClass cc = new CategoryClass.CategoryBuilder(cat.name)
-                .description(cat.description)
-                .build();
+    public void addCategory(@RequestBody String name, @RequestBody String description) {
+        CategoryClass category  = new CategoryBuilder(name).description(description).build();
+        return categoryRepository.save(new Category(category.getName(),category.getDescription());
     }
+
+
 }
